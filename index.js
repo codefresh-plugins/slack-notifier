6cd0afa040d1576061d2b937ad0c76c1cf4d0a9d
pasha <pavel@codefresh.io>
2018-12-10 18:34:41 +0200

    format readme

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const SlackWebhook = require('slack-webhook');
 
-const _ = require('lodash');
-
-
-if(!process.env.SLACK_HOOK_URL){
+if (!process.env.SLACK_HOOK_URL) {
     console.error('SLACK_HOOK_URL env variable should be present');
     process.exit(1);
 }
@@ -12,14 +9,14 @@ const slack = new SlackWebhook(process.env.SLACK_HOOK_URL);
 
 const attachments = process.env.ATTACHMENTS ? JSON.parse(process.env.ATTACHMENTS) : undefined;
 
-if(!process.env.SLACK_TEXT && !attachments){
+if (!process.env.SLACK_TEXT && !attachments) {
     console.error('SLACK_TEXT or ATTACHMENTS env variable should be present');
     process.exit(1);
 }
 
 slack.send({
-  text: process.env.SLACK_TEXT,
-  attachments: attachments,
-  username: process.env.SLACK_USER_NAME,
-  icon_emoji: process.env.SLACK_ICON_EMOJI,
+    text: process.env.SLACK_TEXT,
+    attachments: attachments,
+    username: process.env.SLACK_USER_NAME,
+    icon_emoji: process.env.SLACK_ICON_EMOJI,
 });
