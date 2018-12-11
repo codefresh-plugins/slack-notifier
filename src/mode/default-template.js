c1fa77a9a5abb940c212dcbe5919855629e108ca
pasha <pavel@codefresh.io>
2018-12-11 15:34:40 +0200

    change images

--- src/mode/default-template.js
+++ src/mode/default-template.js
@@ -7,8 +7,8 @@ const template = {
             "pretext":"Image pushed to repository",
             "author_name": process.env.CF_COMMIT_AUTHOR,
             "author_link":"https://cloudposse.com/wp-content/uploads/sites/29/2018/02/small-cute-robot-square.png",
-            "author_icon":"https://cloudposse.com/wp-content/uploads/sites/29/2018/02/small-cute-robot-square.png",
-            "thumb_url":"https://cloudposse.com/wp-content/uploads/sites/29/2018/02/SquareLogo2.png",
+            "author_icon":"https://g.codefresh.io/modules/cf.resources/images/codefresh.png",
+            "thumb_url":"https://codefresh.io/docs/assets/brand/codefresh-social.png",
             "footer":"test",
             "fields":[
                 {
@@ -27,6 +27,11 @@ const template = {
 class DefaultTemplateMode {
 
     static send() {
+
+        if(process.env.CF_BRANCH){
+            console.error('Scm information doesnt present, this mode allow only for repo pipelines or pipelines that call use git trigger');
+            process.exit(1);
+        }
         console.log('Choose default-template mode');
 
         SlackApi.send(process.env.SLACK_TEXT, [template], process.env.SLACK_USER_NAME, process.env.SLACK_ICON_EMOJI);
